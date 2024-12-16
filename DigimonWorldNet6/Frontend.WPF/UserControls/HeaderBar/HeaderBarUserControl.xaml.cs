2b6e9817dd673ef93721b17263a3963a7dec5d65
Gino <ginoshie@gmail.com>
2024-12-16 20:54:17 +0100

    Moved the header bar to a user control to simplify the main window and improve reusability.

---
+++ DigimonWorldNet6/Frontend.WPF/UserControls/HeaderBar/HeaderBarUserControl.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace DigimonWorld.Frontend.WPF.UserControls.HeaderBar;
+
+public partial class HeaderBarUserControl : UserControl
+{
+    public HeaderBarUserControl()
+    {
+        InitializeComponent();
+    }
+}
