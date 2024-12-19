6c9ee9c0e29c4284679704026ba63c157620c84d
Gino <ginoshie@gmail.com>
2024-12-19 20:56:21 +0100

    Almost there. the window is opening, just needs to close now.

--- DigimonWorldNet6/Frontend.WPF/UserControls/FlaggedEvolutionPicker/FlaggedEvolutionPickerViewModel.cs
+++ DigimonWorldNet6/Frontend.WPF/UserControls/FlaggedEvolutionPicker/FlaggedEvolutionPickerViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Generics.Enums;
@@ -15,14 +16,95 @@ public class FlaggedEvolutionPickerViewModel : INotifyPropertyChanged
         set
         {
             if (_componentIsOpen == value) return;
-            
+
             _componentIsOpen = value;
             OnPropertyChanged();
         }
     }
 
+    public ObservableCollection<string> FreshStageIcons { get; } =
+    [
+        "Images/Icons/Digimon/botamon-icon.jpg",
+        "Images/Icons/Digimon/poyomon-icon.jpg",
+        "Images/Icons/Digimon/punimon-icon.jpg",
+        "Images/Icons/Digimon/yuramon-icon.jpg"
+    ];
+
+    public ObservableCollection<string> InTrainingStageIcons { get; } =
+    [
+        "Images/Icons/Digimon/koromon-icon.jpg",
+        "Images/Icons/Digimon/tokomon-icon.jpg",
+        "Images/Icons/Digimon/tsunomon-icon.jpg",
+        "Images/Icons/Digimon/tanemon-icon.jpg"
+    ];
+
+    public ObservableCollection<string> RookieStageIcons { get; } =
+    [
+        "Images/Icons/Digimon/agumon-icon.jpg",
+        "Images/Icons/Digimon/gabumon-icon.jpg",
+        "Images/Icons/Digimon/patamon-icon.jpg",
+        "Images/Icons/Digimon/elecmon-icon.jpg",
+        "Images/Icons/Digimon/biyomon-icon.jpg",
+        "Images/Icons/Digimon/kunemon-icon.jpg",
+        "Images/Icons/Digimon/palmon-icon.jpg",
+        "Images/Icons/Digimon/betamon-icon.jpg",
+        "Images/Icons/Digimon/penguinmon-icon.jpg"
+    ];
+
+    public ObservableCollection<string> ChampionStageIcons { get; } =
+    [
+        "Images/Icons/Digimon/greymon-icon.jpg",
+        "Images/Icons/Digimon/monochromon-icon.jpg",
+        "Images/Icons/Digimon/ogremon-icon.jpg",
+        "Images/Icons/Digimon/airdramon-icon.jpg",
+        "Images/Icons/Digimon/kuwagamon-icon.jpg",
+        "Images/Icons/Digimon/whamon-icon.jpg",
+        "Images/Icons/Digimon/frigimon-icon.jpg",
+        "Images/Icons/Digimon/nanimon-icon.jpg",
+        "Images/Icons/Digimon/meramon-icon.jpg",
+        "Images/Icons/Digimon/drimogemon-icon.jpg",
+        "Images/Icons/Digimon/leomon-icon.jpg",
+        "Images/Icons/Digimon/kokatorimon-icon.jpg",
+        "Images/Icons/Digimon/vegiemon-icon.jpg",
+        "Images/Icons/Digimon/shellmon-icon.jpg",
+        "Images/Icons/Digimon/mojyamon-icon.jpg",
+        "Images/Icons/Digimon/birdramon-icon.jpg",
+        "Images/Icons/Digimon/tyrannomon-icon.jpg",
+        "Images/Icons/Digimon/angemon-icon.jpg",
+        "Images/Icons/Digimon/unimon-icon.jpg",
+        "Images/Icons/Digimon/ninjamon-icon.jpg",
+        "Images/Icons/Digimon/coelamon-icon.jpg",
+        "Images/Icons/Digimon/numemon-icon.jpg",
+        "Images/Icons/Digimon/centarumon-icon.jpg",
+        "Images/Icons/Digimon/devimon-icon.jpg",
+        "Images/Icons/Digimon/bakemon-icon.jpg",
+        "Images/Icons/Digimon/kabuterimon-icon.jpg",
+        "Images/Icons/Digimon/seadramon-icon.jpg",
+        "Images/Icons/Digimon/garurumon-icon.jpg",
+        "Images/Icons/Digimon/sukamon-icon.jpg"
+    ];
+
+    public ObservableCollection<string> UltimateStageIcons { get; } =
+    [
+        "Images/Icons/Digimon/metalgreymon-icon.jpg",
+        "Images/Icons/Digimon/skullgreymon-icon.jpg",
+        "Images/Icons/Digimon/giromon-icon.jpg",
+        "Images/Icons/Digimon/h-kabuterimon-icon.jpg",
+        "Images/Icons/Digimon/mamemon-icon.jpg",
+        "Images/Icons/Digimon/megaseadramon-icon.jpg",
+        "Images/Icons/Digimon/vademon-icon.jpg",
+        "Images/Icons/Digimon/etemon-icon.jpg",
+        "Images/Icons/Digimon/andromon-icon.jpg",
+        "Images/Icons/Digimon/megadramon-icon.jpg",
+        "Images/Icons/Digimon/phoenixmon-icon.jpg",
+        "Images/Icons/Digimon/piximon-icon.jpg",
+        "Images/Icons/Digimon/metalmamemon-icon.jpg",
+        "Images/Icons/Digimon/monzaemon-icon.jpg",
+        "Images/Icons/Digimon/digitamamon-icon.jpg",
+    ];
+
     public IEnumerable<DigimonType> FlaggedDigimonTypes { get; set; } = [];
-    
+
     public event PropertyChangedEventHandler? PropertyChanged;
 
     private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
--- DigimonWorldNet6/Frontend.WPF/Windows/MainWindow/MainWindowViewModel.cs
+++ DigimonWorldNet6/Frontend.WPF/Windows/MainWindow/MainWindowViewModel.cs
@@ -1,30 +1,50 @@
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Input;
 
 namespace DigimonWorld.Frontend.WPF.Windows.MainWindow;
 
-public class MainWindowViewModel
+public class MainWindowViewModel : INotifyPropertyChanged
 {
+    private bool _leftPaneIsOpen;
+
     public MainWindowViewModel(Window window)
     {
         MinimizeCommand = new CommandHandler(() => window.WindowState = WindowState.Minimized);
-        
+
         CloseCommand = new CommandHandler(window.Close);
 
         DragCommand = new CommandHandler(() => DragWindow(window));
+        
+        ToggleLeftPaneCommand = new CommandHandler(ToggleLeftPane);
     }
-    
+
+    public bool LeftPaneIsOpen
+    {
+        get => _leftPaneIsOpen;
+        private set
+        {
+            if (_leftPaneIsOpen == value) return;
+
+            _leftPaneIsOpen = value;
+            OnPropertyChanged();
+        }
+    }
+
     public ICommand MinimizeCommand { get; }
-    
+
     public ICommand CloseCommand { get; }
-    
+
     public ICommand DragCommand { get; }
+    
+    public ICommand ToggleLeftPaneCommand { get; }
 
     private void DragWindow(Window window)
     {
         if (Mouse.PrimaryDevice.LeftButton != MouseButtonState.Pressed) return;
-        
+
         try
         {
             window.DragMove();
@@ -33,5 +53,17 @@ public class MainWindowViewModel
         {
             // Ignore exceptions caused by improper DragMove calls
         }
+    }
+    
+    private void ToggleLeftPane()
+    {
+        LeftPaneIsOpen = !LeftPaneIsOpen;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
