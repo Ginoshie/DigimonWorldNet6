5909b3d160ad0fb9c45dbe267a4c0809b47e0a89
Gino <ginoshie@gmail.com>
2025-01-19 15:17:47 +0100

    Moved more components to user controls and resource dictionaries in order to isolate elements and reduce the size of user controls. This also prevent a bunch of repetitive code the HistoricEvolutionPicker.

---
+++ DigimonWorldNet6/Frontend.WPF/UserControls/EvolutionTool/UserControls/JijimonNaratorTextDisplay.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace DigimonWorld.Frontend.WPF.UserControls.EvolutionTool.UserControls;
+
+public partial class JijimonNaratorTextDisplay : UserControl
+{
+    public JijimonNaratorTextDisplay()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DigimonWorldNet6/Frontend.WPF/UserControls/HistoricEvolutionPicker/UserControls/HistoricEvolutionButton.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace DigimonWorld.Frontend.WPF.UserControls.HistoricEvolutionPicker.UserControls;
+
+public partial class HistoricEvolutionButton : UserControl
+{
+    public HistoricEvolutionButton()
+    {
+        InitializeComponent();
+    }
+}
