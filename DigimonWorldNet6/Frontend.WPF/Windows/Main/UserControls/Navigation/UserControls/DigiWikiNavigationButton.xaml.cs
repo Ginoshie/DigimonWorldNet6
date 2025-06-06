820590228f0e57fc09f7893279f3bf1a2f224e37
Gino <ginoshie@gmail.com>
2025-06-06 21:03:10 +0200

    Removed some obsolete code.

--- DigimonWorldNet6/Frontend.WPF/Windows/Main/UserControls/Navigation/UserControls/DigiWikiNavigationButton.xaml.cs
+++ DigimonWorldNet6/Frontend.WPF/Windows/Main/UserControls/Navigation/UserControls/DigiWikiNavigationButton.xaml.cs
@@ -1,8 +1,6 @@
-using System.Windows.Controls;
-
 namespace DigimonWorld.Frontend.WPF.Windows.Main.UserControls.Navigation.UserControls;
 
-public partial class DigiWikiNavigationButton : UserControl
+public partial class DigiWikiNavigationButton
 {
     public DigiWikiNavigationButton()
     {
--- DigimonWorldNet6/Frontend.WPF/Windows/Main/UserControls/Navigation/UserControls/EvolutionCalculatorNavigationButton.xaml.cs
+++ DigimonWorldNet6/Frontend.WPF/Windows/Main/UserControls/Navigation/UserControls/EvolutionCalculatorNavigationButton.xaml.cs
@@ -1,8 +1,6 @@
-using System.Windows.Controls;
-
 namespace DigimonWorld.Frontend.WPF.Windows.Main.UserControls.Navigation.UserControls;
 
-public partial class EvolutionCalculatorNavigationButton : UserControl
+public partial class EvolutionCalculatorNavigationButton
 {
     public EvolutionCalculatorNavigationButton()
     {
